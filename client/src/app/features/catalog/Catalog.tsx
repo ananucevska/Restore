73e7923bc3673860468d7e684d12cdb058cd9192
Ana <anna_nucevska@hotmail.com>
2025-04-27 17:25:41 +0200

    End of section 3

---
+++ client/src/app/features/catalog/Catalog.tsx
@@ -0,0 +1,13 @@
+﻿import { Product } from "../../models/product";
+import ProductList from "./ProductList";
+
+type Props = {
+    products: Product[];
+}
+export default function Catalog({products}: Props) {
+    return (
+        <>
+            <ProductList products={products}></ProductList>
+        </>
+   )
+}
